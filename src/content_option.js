8f03f7bc15501d11d2b590bdb8b9e84d5cca9067
bravo9798 <bhuvneshsharma9798@gmail.com>
2025-05-17 19:04:10 +0530

    Initial commit for my portfolio

--- src/content_option.js
+++ src/content_option.js
@@ -1,166 +1,219 @@
-const logotext = "JOHN";
+import fwd from "../src/assets/images/fwd.png";
+import animalmerch from "../src/assets/images/animalmerch.png";
+import ultroneous from "../src/assets/images/ultroneous.png";
+import sleepwell from "../src/assets/images/sleepwell.png";
+import grammy from "../src/assets/images/grammy.png";
+import doordash from "../src/assets/images/doordash.png";
+import khulke from "../src/assets/images/khulke.png";
+
+const logotext = "Full Stack Developer";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+  title: "Bhuvnesh Sharma",
+  description:
+    "I’m Bhuvnesh Sharma data Full stack devloper,currently working in Ahmedabad",
 };
 
 const introdata = {
-    title: "I’m John Doe",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
-    },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+  title: "I’m Bhuvnesh Sharma",
+  animated: {
+    first: "I thrive on unraveling complex problems through code.",
+    second: "Crafting innovative websites is my passion.",
+    third: "Building captivating digital experiences brings me joy.",
+  },
+  description:
+    "With over 4 years of industry expertise, I am an experienced Full Stack Developer. Renowned for superior client engagement abilities, I effectively grasp and address client needs. Whether working autonomously or within team environments, I consistently achieve timely project delivery with a flawless 100% on-time track record.",
+  // your_img_url: "https://images.pexels.com/photos/5380661/pexels-photo-5380661.jpeg",
+  your_img_url: "https://images.pexels.com/photos/5380590/pexels-photo-5380590.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+  title: "abit about my self",
+  aboutme:
+    "Outside of work, I'm passionate about fitness, travel, and socializing. Whether it's hitting the gym, exploring new destinations, or simply enjoying good company, these activities enrich my life and keep me balanced",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+const worktimeline = [
+  {
+    jobtitle: "Software Engineer",
+    where: "Nstack Softech",
+    date: "2021",
+  },
+  {
+    jobtitle: "Frontend Developer",
+    where: "Ultroneous Technologies",
+    date: "2022",
+  },
+  {
+    jobtitle: "Frontend Developer",
+    where: "Domain of seven stalions",
+    date: "2023",
+  },
+  {
+    jobtitle: "Software Engineer",
+    where: "Tirex Chargers",
+    date: "2024",
+  },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
-    },
+const skills = [
+  {
+    name: "Reactjs",
+    value: 90,
+  },
+  {
+    name: "HTML",
+    value: 85,
+  },
+  {
+    name: "CSS",
+    value: 85,
+  },
+  {
+    name: "Python",
+    value: 55,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "Typescript",
+    value: 70,
+  },
+  {
+    name: "Nextjs",
+    value: 85,
+  },
+  {
+    name: "GraphQl",
+    value: 70,
+  },
+  {
+    name: "Nodejs",
+    value: 75,
+  },
+  {
+    name: "Redux",
+    value: 70,
+  },
+  {
+    name: "ChakraUI",
+    value: 90,
+  },
+  {
+    name: "TailwindCSS",
+    value: 90,
+  },
+  {
+    name: "Bootstrap",
+    value: 80,
+  },
+  {
+    name: "Flask",
+    value: 70,
+  },
+  {
+    name: "React Native",
+    value: 65,
+  },
+  {
+    name: "SQL",
+    value: 70,
+  },
+  {
+    name: "MongoDB",
+    value: 60,
+  },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+const services = [
+  {
+    title: "Frontend Development",
+    description:
+      "As a seasoned frontend developer, I've navigated numerous projects independently, showcasing expertise in designing dynamic and browser-compatible pages using an array of technologies including React, Next.js, HTML, Chakra UI, and CSS. Proficient in state management, I utilize Redux to ensure seamless data flow within applications.My forte lies in crafting responsive UI components and layouts, adept at translating design requirements from Figma and PSD into code. Functionality meets aesthetics as I collaborate with teams to implement smooth, performance-driven interfaces utilizing React concepts.With a proven track record as a team leader, I've orchestrated project flows, documented MVPs and PRDs, and fostered an agile, collaborative environment. API integrations are seamless thanks to my experience with tools like Axios, GraphQL, and Apollo Client.Beyond development, I'm a proactive team player with excellent communication skills, adept at collaborating with developers to implement new features and maintain existing web applications. Client-facing interactions are second nature, evident through successful client interviews and project deployments.",
+  },
+  {
+    title: "Project Management",
+    description:
+      "In my capacity as a project manager, I have spearheaded various initiatives to streamline project workflows and ensure successful outcomes. I've played a pivotal role in crafting comprehensive project requirement documents (PRD), software requirement specifications (SRS), and system design documents (SDD), facilitating clear communication between clients and our development team. Leading a team of developers and designers, I have fostered a collaborative environment, incorporating their expertise and facilitating seamless cooperation to meet project goals efficiently. My adept client interaction skills have enabled me to grasp client needs effectively, translating their ideas into actionable plans such as minimum viable products (MVP) and detailed PRDs. Leveraging project management tools such as Jira, Atlassian, Asana, and Slack, I have orchestrated project timelines and resources with precision, ensuring timely deliverables and effective communication channels. Furthermore, I have contributed to the project's success by creating user flows for applications, architecting comprehensive frameworks, and meticulously selecting the most suitable technology stacks to align with project requirements and optimize performance.",
+  },
+  {
+    title: "Backend Development",
+    description:
+      "In the realm of backend development, I excel in crafting robust solutions fueled by a profound understanding of database design, implementation, and optimization, with a particular emphasis on MongoDB and SQL. My expertise extends to proficiently designing and developing RESTful APIs using Node.js and Express.js, seamlessly integrating backend functionalities with contemporary frontend build pipelines and tools. Adept at debugging and resolving issues within existing codebases, I ensure the reliability and seamless operation of applications.A collaborative team player, I thrive in cross-functional environments, actively contributing to the definition, design, and successful delivery of new features. With a diverse experience encompassing the creation of websocket, SOAP, and REST APIs, complemented by a proven track record of effectively managing projects through short-term sprints and long-term deployments, I bring a comprehensive approach to backend development. Leveraging deployment tools such as Docker and AWS, alongside rigorous unit testing, I streamline deployment processes to ensure the dependable delivery of applications.",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: fwd,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://www.fwd.com/en/",
+  },
+  {
+    img: ultroneous,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://ultroneous.com/",
+  },
+  {
+    img: animalmerch,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://animalmerch.com/",
+  },
+  {
+    img: sleepwell,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://mysleepwell.com/",
+  },
+  {
+    img: grammy,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://www.grammy.com/",
+  },
+  {
+    img: doordash,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://www.doordash.com/",
+  },
 
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+  {
+    img: khulke,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://www.khulke.com/",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "bhuvneshsharma9798@gmail.com",
+  YOUR_FONE: "(+91)9316227008",
+  description:
+    "Reach out to me directly through this contact form to discuss collaborations, opportunities, or any inquiries you may have. I look forward to connecting with you!",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+  github: "https://github.com/bravo9798",
+  linkedin: "https://www.linkedin.com/in/bhuvnesh-sharma-00985b1bb/",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
